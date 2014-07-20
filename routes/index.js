d9699d77a941ad8464f26e07425eceaddeded47b
yuchunyu <yuchunyu@goyoo.com>
2014-07-20 21:04:34 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var pageUser = require('../lib/pageUser');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'cloud foundry' });
+  res.render('login', { title: 'Express' });
 });
+
+router.post('/login', pageUser.login);
 
 module.exports = router;
