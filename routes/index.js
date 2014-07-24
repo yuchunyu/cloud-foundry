271a462beffc8bbe3995a8c3f9d933bf1e9ebdbf
yuchunyu <yuchunyu@goyoo.com>
2014-07-24 22:15:06 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,20 @@ var pageUser = require('../lib/pageUser');
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  res.render('login', { title: 'Express' });
+});
+router.get('/manage', function(req, res) {
   res.render('manage', { title: 'Express' });
 });
-
-router.post('/login', pageUser.login);
+router.get('/logout', function(req, res) {
+  res.render('login', { title: 'Express' });
+});
+router.get('/index', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/*', function(req, res) {
+  res.render('login', { title: 'Express' });
+});
+// router.post('/login', pageUser.login);
 
 module.exports = router;
