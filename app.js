aa6b4ffaf590944f292de8d3c48c38b065f97ed4
yuchunyu <yuchunyu@goyoo.com>
2014-08-28 23:52:03 +0800

    update

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
+var routes = require('./lib/route');
 
 var app = express();
 
